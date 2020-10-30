5fd88170d8290c4489f932a5209efe1fb80cd037
Yuyang Zhou <yuyang_zhou@berkeley.edu>
2020-10-30 07:09:57 -0700

    checkpoint

--- generate_maps.py
+++ generate_maps.py
@@ -2,61 +2,86 @@ import numpy as np
 import healpy as hp
 import argparse
 
-parser = argparse.ArgumentParser(
+def parse_arguments():
+    parser = argparse.ArgumentParser(
         description="Generate 1/l^2 spectrum E/B maps for matrix purification study",
         fromfile_prefix_chars="@",
     )
 
-parser.add_argument(
-    '--nside',
-    required=False,
-    default=512,
-    help='nside of map',
-)
+    parser.add_argument(
+        '--nside',
+        required=False,
+        default=64,
+        help='nside of map',
+    )
 
-parser.add_argument(
-    '--mode',
-    required=True,
-    help='E or B'
-)
+    parser.add_argument(
+        '--mode',
+        required=False,
+        default='both',
+        help='E or B or both'
+    )
 
-parser.add_argument(
-    '--nreal',
-    required=False,
-    default=1,
-    help='Number of maps'
-)
+    parser.add_argument(
+        '--nreal',
+        required=False,
+        type=np.int8,
+        default=3,
+        help='Number of maps'
+    )
 
-parser.add_argument(
-    '--lmax',
-    required=False,
-    default=100,
-    help='ell max'
-)
+    parser.add_argument(
+        '--lmax',
+        required=False,
+        default=100,
+        help='ell max'
+    )
+    args = parser.parse_args()
+    return args
 
-parser.add_argument(
-    '--outpath',
-    required=True,
-    help='out path'
-)
+def make_cl(args, mode):
+    cl_TT = [0 for l in range(2, args.lmax)]
+    if mode == 'B':
+        cl_EE = [0 for l in range(2, args.lmax)]
+        cl_BB = [1/l**2 for l in range(2, args.lmax)]
+    if mode == 'E':
+        cl_EE = [1/l**2 for l in range(2, args.lmax)]
+        cl_BB = [0 for l in range(2, args.lmax)]
+    cl_TE = [0 for l in range(2, args.lmax)]
+    cl = cl_TT, cl_EE, cl_BB, cl_TE
+    for c in cl:
+        for i in range(2):
+            c.insert(0,0)
+    return cl
 
-args = parser.parse_args()
+def make_maps(args, cl, mode):
+    for i in range(args.nreal):
+        print(f'Processing {mode}, realization {i}')
+        alms = hp.synalm(cl, new=True)
+        m = hp.alm2map(alms, args.nside, pol=True)
+        hp.write_map(f"./input_maps/{mode}/{mode}_map_{i}.fits", m, overwrite=True, nest= True, dtype=np.float32)
 
-cl_TT = [0 for l in range(2, args.lmax)]
-if args.mode == 'B':
-    cl_EE = [0 for l in range(2, args.lmax)]
-    cl_BB = [1/l**2 for l in range(2, args.lmax)]
-if args.mode == 'E':
-    cl_EE = [1/l**2 for l in range(2, args.lmax)]
-    cl_BB = [0 for l in range(2, args.lmax)]
-cl_TE = [0 for l in range(2, args.lmax)]
-cl = cl_TT, cl_EE, cl_BB, cl_TE
-for c in cl:
-    for i in range(2):
-        c.insert(0,0)
+def make_E_maps(args):
+    mode = 'E'
+    cl = make_cl(args, mode)
+    make_maps(args, cl, mode)
         
-for i in range(args.nreal):
-    print(f'Processing realization {i}')
-    alms = hp.synalm(cl, new=True)
-    m = hp.alm2map(alms, args.nside, pol=True)
-    hp.write_map(f"{args.outpath}/{args.mode}_map_{i}.fits", m, overwrite=True, dtype=np.float32)
+def make_B_maps(args):
+    mode = 'B'
+    cl = make_cl(args, mode)
+    make_maps(args, cl, mode)
+    
+def main():
+    
+    args=parse_arguments()
+    
+    if args.mode=='E':
+        make_E_maps(args)
+    if args.mode=='B':
+        make_B_maps(args)
+    if args.mode=='both':
+        make_E_maps(args)
+        make_B_maps(args)
+    
+if __name__ == "__main__":
+    main()
